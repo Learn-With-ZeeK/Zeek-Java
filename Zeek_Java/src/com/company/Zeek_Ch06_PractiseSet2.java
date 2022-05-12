16c82128de97f1adaae0122ce3b4d8df3ce3c9bc
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2022-05-12 22:57:50 +0530

    Create an array and write a code to find its maximum element.
    Based on concepts from { Zeek_27_ArrayStarting.java } to { Zeek_28_MultiDimensionArray.java }

--- Zeek_Java/src/com/company/Zeek_Ch06_PractiseSet2.java
+++ Zeek_Java/src/com/company/Zeek_Ch06_PractiseSet2.java
@@ -7,7 +7,7 @@ public class Zeek_Ch06_PractiseSet2 {
 	int[] cost= {23,24,27,28,21,39,34,36,31,48,43,42,47,41};
 	
 	int max=0;
-	for(int e:cost) {
+	 for(int e:cost) {
 		if(e>max) {
 			max=e;
 		}
